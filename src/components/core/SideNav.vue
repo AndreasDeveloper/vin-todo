8034591c7182e82d1708bf19298a1d5ed38f27c3
unknown <andreasmrdja16@gmail.com>
2020-09-21 13:22:37 +0200

    Improved styles on global side nav component.

--- src/components/core/SideNav.vue
+++ src/components/core/SideNav.vue
@@ -2,7 +2,7 @@
   <nav class="navbar">
     <ul class="navbar-nav">
       <li class="logo">
-        <a href="/" class="nav-link">
+        <div class="nav-link logo-nav-link">
           <span class="link-text logo-text vin-font">Vin</span>
           <svg
             aria-hidden="true"
@@ -27,7 +27,7 @@
               />
             </g>
           </svg>
-        </a>
+        </div>
       </li>
 
       <li class="nav-item">
